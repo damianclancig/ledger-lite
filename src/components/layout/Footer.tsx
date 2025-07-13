7169bb70f831da83da6b5a9741b92a75625b8e6b
Damián Clancig <damianclancig@gmail.com>
2025-07-13 05:05:55 +0000

    fix: Remove link license & justify-between footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,6 @@
 
 "use client";
 
-import Link from "next/link";
 import { Github } from "lucide-react";
 
 export function Footer() {
@@ -9,30 +8,21 @@ export function Footer() {
 
   return (
     <footer className="w-full border-t bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex flex-col items-center justify-between gap-4 py-5 md:h-20 md:flex-row md:py-0">
-        <div className="flex flex-col items-center gap-4 px-8 md:gap-2">
-          <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
-            © {currentYear} Ledger Lite. All rights reserved. Released under the{" "}
-            <Link
-              href="/LICENSE"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              MIT License
-            </Link>
-            .
+      <div className="w-full px-4 sm:px-6 lg:px-8">
+        <div className="flex h-20 items-center justify-between">
+          <p className="text-sm text-muted-foreground">
+            © {currentYear} Ledger Lite. All rights reserved. Released under the MIT License.
           </p>
-        </div>
-         <a
+          <a
             href="https://github.com/damianclancig/ledger-lite"
             target="_blank"
             rel="noreferrer"
-            className="font-medium underline underline-offset-4"
+            className="font-medium underline-offset-4"
           >
             <Github className="h-6 w-6 text-primary hover:text-accent transition-colors" />
             <span className="sr-only">GitHub</span>
           </a>
+        </div>
       </div>
     </footer>
   );
